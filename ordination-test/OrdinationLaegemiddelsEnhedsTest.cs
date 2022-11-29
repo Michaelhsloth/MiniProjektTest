b96997a5cd3ca6a64b18b19d7a774467d02291b0
M.H.S <michael.h.sloth@gmail.com>
2022-11-29 11:27:26 +0100

    flere tests

--- ordination-test/OrdinationLaegemiddelsEnhedsTest.cs
+++ ordination-test/OrdinationLaegemiddelsEnhedsTest.cs
@@ -1,6 +1,7 @@
 namespace ordination_test;
 
 using shared.Model;
+using static shared.Util;
 
 
 
@@ -15,9 +16,9 @@ public class OrdinationLaegemiddelsEnhedsTest
     {
         Laegemiddel laegemiddler = new Laegemiddel();
 
-        
+
         laegemiddler = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
-        
+
 
 
         DateTime startDato = new DateTime(2021, 1, 10);
@@ -31,5 +32,48 @@ public class OrdinationLaegemiddelsEnhedsTest
 
         Assert.AreEqual(laegemiddler, dagligFast.laegemiddel);
     }
+    [TestMethod]
+    public void DagligSkævRigtigLaegeMiddel()
+    {
+        Laegemiddel laegemiddler = new Laegemiddel();
+
+
+        laegemiddler = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        Dosis[] dosis = new Dosis[4];
+        dosis[0] = new Dosis(CreateTimeOnly(15, 0, 0), 7);
+        dosis[1] = new Dosis(CreateTimeOnly(18, 0, 0), 9);
+        dosis[2] = new Dosis(CreateTimeOnly(22, 0, 0), 3);
+        dosis[3] = new Dosis(CreateTimeOnly(23, 0, 0), 1);
+
+
+        DateTime startDato = new DateTime(2021, 1, 10);
+        DateTime slutDato = new DateTime(2021, 1, 12);
+        Laegemiddel laegemiddel = laegemiddler;
+
+        DagligSkæv dagligSkæv = new DagligSkæv(startDato, slutDato, laegemiddel, dosis);
+
+        Assert.AreEqual(laegemiddler, dagligSkæv.laegemiddel);
+    }
+
+    [TestMethod]
+
+    public void DagligPNRigtigLaegeMiddel()
+    {
+        Laegemiddel laegemiddler = new Laegemiddel();
+
+        laegemiddler = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+
+        DateTime startDato = new DateTime(2021, 1, 10);
+        DateTime slutDato = new DateTime(2021, 1, 12);
+        double antal = 2;
+        Laegemiddel laegemiddel = laegemiddler;
+
+        PN dagligPN = new PN(startDato, slutDato, antal, laegemiddel);
+
+        Assert.AreEqual(laegemiddler, dagligPN.laegemiddel);
+    }
+
+
+
 
 }
